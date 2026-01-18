5c2fa6439111a04076e2a8398904d950c1ccdbc8
RavinduPerera <it22064622@my.sliit.lk>
2026-01-18 18:50:21 +0530

    set the new folder structure

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to the Hoby_Track</Text>
     </View>
   );
 }
